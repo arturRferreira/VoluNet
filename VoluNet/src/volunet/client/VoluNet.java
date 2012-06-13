1f69a62403cf7f1dfd860be935a769e7bea37303
Artur Ferreira <arturRferreira@sapo.pt>
2012-06-13 12:16:00 +0100

    new

--- VoluNet/src/volunet/client/VoluNet.java
+++ VoluNet/src/volunet/client/VoluNet.java
@@ -41,7 +41,7 @@ public class VoluNet implements EntryPoint {
 	public void onModuleLoad() {
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		nameField.setText("Insert your name here");
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
